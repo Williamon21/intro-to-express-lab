77f69fe52a2abf4cf74fc5ebc1a9b44386eb1f9c
dlswilliam <santo@Project2.localdomain>
2026-01-07 02:18:15 -0500

    real head scratcher

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const collectibles = [
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
+  const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
 
 // Define routes here (we'll add them soon)
 
@@ -31,14 +40,37 @@ app.get('/roll/:number', (req, res) => {
 });
 
 //exercise 3
-app.get('/collectibles/:index', (req, res) => {
-    const index = req.params.index;
-    if(req.query.q) res.send(`You searched for: ${req.query.q}`);
-    if (!req.query.q) res.send("Please add a query param of 'q'")
-});
-// Listen for requests on port 3000
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+
+  app.get('/collectibles/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10)
+    const result = collectibles[index]
+
+    if (result) {
+        res.send(`So, you want the ${result.name}? For ${result.price}, it can be yours!`)
+    } else {
+        res.send('This item is not yet in stock. Check back soon!')
+    }
 })
 
+//exercise 4
+app.get('/shoes', (req, res) => {
+    const minPrice = req.query['min-price']
+    const maxPrice = req.query['max-price']
+    const type = req.query.type
 
+    let filteredShoes = shoes
+
+    if (minPrice) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.price >= parseFloat(minPrice))
+    }else if (maxPrice) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.price <= parseFloat(maxPrice))
+    }else if (type) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.type === type)
+    }
+
+    res.send(filteredShoes)
+    })
+
+app.listen(3000, ( () => {
+    console.log('server is running on port 3000')
+}))
